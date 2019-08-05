b39dda157b76df62c28c48b120f82c082355ff97
Robbert Broersma <mail@robbertbroersma.nl>
2019-08-05 08:00:00 +0200

    DELETE ME: Configure token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 import { IEnvironment } from './environment.model';
 
 export const environment: IEnvironment = {
+    openWeatherMapsToken: 'c4dea85fee8dcaa7b49f0387576e040f',
     production: false,
 };
