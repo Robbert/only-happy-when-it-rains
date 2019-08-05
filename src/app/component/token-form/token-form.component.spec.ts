81199ca4bb8a0b989940b933a956be74cd93f766
Robbert Broersma <mail@robbertbroersma.nl>
2019-08-05 08:00:00 +0200

    Define expectations for the token form

---
+++ src/app/component/token-form/token-form.component.spec.ts
@@ -0,0 +1,9 @@
+describe('Form to provide API key', () => {
+    it.skip('should allow the user to enter the token', () => {});
+
+    it.skip('should not visibly display the token to prevent leaking the token', () => {});
+
+    it.skip('should emit the token when the user submits the form', () => {});
+
+    it.skip('should only submit the token when the optional validation step has been successful', () => {});
+});
