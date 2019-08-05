cf1684aaca4f6a3fce2def45b9de54b12bc7b1d6
Robbert Broersma <mail@robbertbroersma.nl>
2019-08-05 08:00:00 +0200

    Initialize Angular using customized `ng new`
    npx --no-install ng new \
        --directory . \
        --force \
        --minimal \
        --routing \
        --skip-git \
        --skip-tests \
        --style scss \
        weather-app
    
    Reverted the changes to:
    
     - `.gitignore`
     - `package.json`
     - `package-lock.json`
    
    Installed dependencies generated by `ng new` with version pinning using:
    
    npm install --save --save-exact \
        @angular/animations@8.1.2 \
        @angular/common@8.1.2 \
        @angular/compiler@8.1.2 \
        @angular/core@8.1.2 \
        @angular/forms@8.1.2 \
        @angular/platform-browser@8.1.2 \
        @angular/platform-browser-dynamic@8.1.2 \
        @angular/router@8.1.2 \
        rxjs@6.4.0 \
        tslib@1.9.0 \
        zone.js@0.9.1
    
    npm install --save-dev --save-exact \
        @angular-devkit/build-angular@0.801.2 \
        @angular/cli@8.1.2 \
        @angular/compiler-cli@8.1.2 \
        @angular/language-service@8.1.2 \
        @types/node@8.9.4 \
        ts-node@7.0.0 \
        tslint@5.15.0 \
        typescript@3.4.3
    
    Removed cruft.
    Added model definition for environment files.
    Set very strict `compilerOptions` in `tsconfig.json`.

---
+++ src/app/polyfills.ts
@@ -0,0 +1,5 @@
+// tslint:disable:ordered-imports
+
+// Zone JS is required by default for Angular itself,
+// Zone JS MUST be included before all others, therefore ignore linting for import order.
+import 'zone.js/dist/zone';
