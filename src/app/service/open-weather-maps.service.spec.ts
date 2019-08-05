185e5c1371821efa1f3e7fbc6d0c7ba0fa7cb462
Robbert Broersma <mail@robbertbroersma.nl>
2019-08-05 08:00:00 +0200

    Define expectations for the weather service

---
+++ src/app/service/open-weather-maps.service.spec.ts
@@ -0,0 +1,27 @@
+describe('Obtaining data from the OpenWeatherMaps API', () => {
+    describe('data for the current weather', () => {
+        it.skip('should use a secure connection to provide the API key', () => {});
+
+        it.skip('should provide the API key via the request URL', () => {});
+
+        it.skip('should provide the temperature for a given location', () => {});
+
+        it.skip('should provide the wind strength for a given location', () => {});
+
+        it.skip('should provide the temperature in Kelvin', () => {});
+
+        it.skip('should provide the wind speed in meters per second', () => {});
+    });
+
+    describe('data for the weather forecast', () => {
+        it.skip('should provide a short-term weather forecast for a given location', () => {});
+
+        it.skip('should provide forecasts for the temperature', () => {});
+
+        it.skip('should provide forecasts for wind speed', () => {});
+
+        it.skip('should provide the temperature in Kelvin', () => {});
+
+        it.skip('should provide the wind speed in meters per second', () => {});
+    });
+});
